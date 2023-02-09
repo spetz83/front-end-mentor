4768c159875df2edebcef1e95f2569dfac2a4182
Tom Etzel <thomas.etzel@gmail.com>
2023-02-09 09:35:24 -0600

    added data fetching using swr

--- pages/challenges/advice.js
+++ pages/challenges/advice.js
@@ -1,18 +1,25 @@
 import Layout from "@/components/layout";
 import styles from "@/styles/Advice.module.css";
-import Image from "next/image";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function Advice() {
+  const { data, mutate } = useSWR("https://api.adviceslip.com/advice", fetcher);
+
+  const getNewAdvice = () => {
+    mutate("https://api.adviceslip.com/advice");
+  };
+
   return (
     <Layout>
       <div className={styles.card}>
-        <h1 className={styles.title}>Advice #117</h1>
-        <p className={styles.advice}>
-          &ldquo;It is easy to sit up and take notice, what&apos;s difficult is
-          getting up and taking action.&rdquo;
-        </p>
+        <h1 className={styles.title}>
+          Advice #{data.slip ? data.slip.id : ""}
+        </h1>
+        <p className={styles.advice}>{data.slip ? data.slip.advice : ""}</p>
         <div className={styles.divider} />
-        <div className={styles.btnContainer}>
+        <div className={styles.btnContainer} onClick={getNewAdvice}>
           <div className={styles.diceBtn}>
             <div className={styles.diceIcon} />
           </div>
